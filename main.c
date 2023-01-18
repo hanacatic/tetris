8a5fa97722d114e99fe513539963e0f4ccef7990
Amer Musinbegovic <114984482+amusinbego1@users.noreply.github.com>
2023-01-18 09:50:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,63 +1,29 @@
-#include <xc.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 
+#include <xc.h>
+#include "max7219_driver.h"
 #pragma config FOSC=HS,WDTE=OFF,PWRTE=OFF,MCLRE=ON,CP=OFF,CPD=OFF,BOREN=OFF,CLKOUTEN=OFF
 #pragma config IESO=OFF,FCMEN=OFF,WRT=OFF,VCAPEN=OFF,PLLEN=OFF,STVREN=OFF,LVP=OFF
-
 #define _XTAL_FREQ 8000000
 
-int a = 0;
-unsigned char counter = 0;
 
-const char figures[7][4] = {
-    {1, 3, 5, 7}, // I
-    {2, 4, 5, 7}, // 4
-    {3, 5, 4, 6}, // N
-    {3, 5, 4, 7}, // T
-    {2, 3, 5, 7}, // L
-    {3, 5, 7, 6}, // J
-    {2, 3, 4, 5}, // O
-};
+void main (void) {
 
-struct Point{
-    char x, y;
-};
-
-//moves figure in x axis depending on dx, dx > 0 RIGHT, dx < 0 LEFT
-void moveFig(struct Point* figure, char dx){
-    for(int i = 0; i < 4; i++){
-        figure[i].x = figure[i].x + dx;
+    TRISD = 0b11100000;
+	
+    // main loop
+    while (1) {
+		
+        MAX7219_send(8, 0b00001111); MAX7219_send(8, 0b00000111); MAX7219_update();
+        MAX7219_send(7, 0b00001111); MAX7219_send(7, 0b00000111); MAX7219_update();
+		MAX7219_send(6, 0b00001111); MAX7219_send(6, 0b00000111); MAX7219_update();
+		MAX7219_send(5, 0b00001111); MAX7219_send(5, 0b00000111); MAX7219_update();
+		MAX7219_send(4, 0b00001111); MAX7219_send(4, 0b00000111); MAX7219_update();
+		MAX7219_send(3, 0b00001111); MAX7219_send(3, 0b00000111); MAX7219_update();
+		MAX7219_send(2, 0b00001111); MAX7219_send(2, 0b00000111); MAX7219_update();
+        MAX7219_send(1, 0b00001111); MAX7219_send(1, 0b00000111); MAX7219_update();
+        
     }
-}
-
-//interrupt routine
-void __interrupt() interr(){
-    if(TMR0IE && TMR0IF){
-        counter = counter + 1;
-    }
-}
-
-//interrupt initialisation
-void interr_init(){
-    //configuration of TMR0 timer
-    TMR0CS = 0;
-    TMR0IF = 0;
-    //using prescaler and setting TMR value to measure time in 1ms
-    PSA = 0;
-    PS0 = 1;
-    PS1 = 0;
-    PS2 = 1;
-    TMR0 = 133;
-    TMR0IE = 1;
-    GIE = 1;
-}
-void main(void) {
-    interr_init();
-    srand(5198);
- 
-    while(1){
-        a = rand() % 7;
-    }
+    
+    return;
+    
 }
