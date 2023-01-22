a0705e3f7ed49f238ee1205cb383ec487b610905
Amer Musinbegovic <114984482+amusinbego1@users.noreply.github.com>
2023-01-22 01:54:28 +0100

    update

--- main.c
+++ main.c
@@ -1,29 +1,62 @@
-
 #include <xc.h>
 #include "max7219_driver.h"
+#include "figure.h"
+
 #pragma config FOSC=HS,WDTE=OFF,PWRTE=OFF,MCLRE=ON,CP=OFF,CPD=OFF,BOREN=OFF,CLKOUTEN=OFF
 #pragma config IESO=OFF,FCMEN=OFF,WRT=OFF,VCAPEN=OFF,PLLEN=OFF,STVREN=OFF,LVP=OFF
 #define _XTAL_FREQ 8000000
 
+char matrix[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+char matrix_row = 3;
+char fig_bin_array[4] = {0,0,0,0};
+char brojac =0;
 
-void main (void) {
+void update_led(void){
+    //Set LED states according to main matrix named 'matrix'.
+    for(char i = 0; i<8; i++){
+        MAX7219_send(8-i, matrix[4+i]);
+        MAX7219_send(8-i, matrix[12+i]);
+        MAX7219_update();
+    }
+}
 
-    TRISD = 0b11100000;
-	
-    // main loop
-    while (1) {
-		
-        MAX7219_send(8, 0b00001111); MAX7219_send(8, 0b00000111); MAX7219_update();
-        MAX7219_send(7, 0b00001111); MAX7219_send(7, 0b00000111); MAX7219_update();
-		MAX7219_send(6, 0b00001111); MAX7219_send(6, 0b00000111); MAX7219_update();
-		MAX7219_send(5, 0b00001111); MAX7219_send(5, 0b00000111); MAX7219_update();
-		MAX7219_send(4, 0b00001111); MAX7219_send(4, 0b00000111); MAX7219_update();
-		MAX7219_send(3, 0b00001111); MAX7219_send(3, 0b00000111); MAX7219_update();
-		MAX7219_send(2, 0b00001111); MAX7219_send(2, 0b00000111); MAX7219_update();
-        MAX7219_send(1, 0b00001111); MAX7219_send(1, 0b00000111); MAX7219_update();
+
+void __interrupt() prekid(void){
+    if(TMR0IF && TMR0IE){
+        INTCONbits.TMR0IF = 0;
+        TMR0 = 180;
+        if (brojac == 100){
+            if(can_go_further(matrix, &matrix_row)){
+                go_down_1place(matrix, &matrix_row, fig_bin_array); update_led();
+            }
+            else{
+                matrix_row = 3; prepare_new_figure(matrix, fig_bin_array);
+            }
+             brojac = 0;
+        }
+        brojac++;
         
     }
+}
+
+void tmr0_initialization(void){
+    //setting TMR0 time to 5 ms.
+    OPTION_REGbits.PS2 = 1;
+    OPTION_REGbits.PS1 = 1;
+    OPTION_REGbits.PS0 = 0;
+    OPTION_REGbits.PSA = 0;
+    OPTION_REGbits.TMR0CS = 0;
+    TMR0 = 180;
+    INTCONbits.TMR0IE = 1;
+    INTCONbits.GIE = 1;
+}
+void main (void) {
+    TRISD = 0b11100000;
+    prepare_new_figure(matrix, fig_bin_array);
+    MAX7219_initialization();
+	tmr0_initialization();
+    // main loop
+    while (1);
     
     return;
-    
 }
