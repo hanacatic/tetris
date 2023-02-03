8e5c418c3c99a4c893db94681207aa95642e64e7
FaikTahirovic <115116624+FaikTahirovic@users.noreply.github.com>
2023-02-03 13:57:29 +0100

    s casa

--- figure.c
+++ figure.c
@@ -25,7 +25,7 @@ char random_number(char max_number){
 
 
 void choose_new_figure(char* fig_bin_array){
-    const char *pom = figures[3/*random_number(7)*/];
+    const char *pom = figures[random_number(7)];
     for(char i=0; i<4; i++){
         fig_bin_array[i] = pom[i];
     }
@@ -46,16 +46,16 @@ __bit can_go_further(char* matrix, char* matrix_row, char* fig_bin_array){
     return (*matrix_row) < 19 && !(fig_bin_array[3] & (matrix[(*matrix_row)+ 1])); 
 }
 
-__bit can_go_right(char* matrix, char* matrix_row, char* fig_bin_array ){
+__bit can_go_right(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array ){
     for(int i = 0; i < 4; i++){
-        if(matrix[*matrix_row - 3 + i] & (1<<7)) return 0;
+        if(((matrix[*matrix_row - 3 + i] & (1<<7))) || ((matrix[*matrix_row - i] ^ fig_bin_array[3-i])&fig_bin_array[3-i]*2)) return 0;
     }
     return 1;
 }
 
-__bit can_go_left(char* matrix, char* matrix_row, char* fig_bin_array){
+__bit can_go_left(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
     for(int i = 0; i < 4; i++){
-        if(matrix[*matrix_row - 3 + i] & (1<<0)) return 0;
+        if((matrix[*matrix_row - 3 + i] & (1<<0)) || ((matrix[*matrix_row - i] ^ fig_bin_array[3-i])&fig_bin_array[3-i]/2)) return 0;
     }
     return 1;
 }
@@ -68,25 +68,47 @@ void go_down_1place(char* matrix, char* matrix_row, char* fig_bin_array){
     (*matrix_row)++;
 }
 
-void go_right(char* matrix, char* matrix_row, char* fig_bin_array){
-    if(can_go_right(matrix, matrix_row, fig_bin_array)){
+void go_right(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
+    if(can_go_right(matrix, matrix_row, matrix_col, fig_bin_array)){
         for(char i=0; i<4; i++){
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i]);
             fig_bin_array[3-i] = fig_bin_array[3-i]*2;
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (fig_bin_array[3-i]);
         }
+        *matrix_col--;
     }
 }
 
-void go_left(char* matrix, char* matrix_row, char* fig_bin_array){
-    if(can_go_left(matrix, matrix_row, fig_bin_array)){
+void go_left(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
+    if(can_go_left(matrix, matrix_row, matrix_col, fig_bin_array)){
         for(char i=0; i<4; i++){
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i]);
             fig_bin_array[3-i] = fig_bin_array[3-i]/2;
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (fig_bin_array[3-i]);
         }
+       *matrix_col ++;
     }
 }
-
-void rotate(char* matrix, char* matrx_row, char* fig_bin_array){
+__bit can_rotate(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
+    if(!*matrix_col && fig_bin_array[1]) return 0;
+}
+void rotate(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
+    if(can_rotate(matrix, matrix_row, matrix_col, fig_bin_array)){
+        char temp = 0, pow;
+        for(char i=0; i<4; i++){
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i]);
+        }
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                temp = temp * 2;
+                temp = temp | ((fig_bin_array[j] & (1 << ((*matrix_col) + i))) << 0);
+            }
+            pow = *matrix_col;
+            while(pow != 0){
+                temp = temp * 2;
+                pow = pow - 1;
+            }
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | temp;
+        }
+    }
 }
