1dbe7ed33b242ddd51c1e56137b3f830ecb72208
hcatic1 <115094201+hcatic1@users.noreply.github.com>
2023-02-10 17:37:56 +0100

    Update figure.c

--- figure.c
+++ figure.c
@@ -3,8 +3,23 @@
 #include <stdlib.h>
 
 
-const char figures[7][4] = 
-{ 
+const char figures[7][4][4] = 
+{   
+    {{0b00000000, 0b00001000, 0b00001000, 0b00011000}, {0b00000000, 0b00000000, 0b00010000, 0b00011100}, {0b00000000, 0b00011000, 0b00010000, 0b00010000}, {0b00000000, 0b00000000, 0b00011100, 0b00010000}},
+    {{0b00000000, 0b00010000, 0b00010000, 0b00011000}, {0b00000000, 0b00000000, 0b00111000, 0b00100000}, {0b00000000, 0b00011000, 0b00001000, 0b00001000}, {0b00000000, 0b00000000, 0b00001000, 0b00111000}},
+    {{0b00010000, 0b00010000, 0b00010000, 0b00010000}, {0b00000000, 0b00000000, 0b00000000, 0b00111100}, {0b00001000, 0b00001000, 0b00001000, 0b00001000}, {0b00000000, 0b00000000, 0b00000000, 0b00111100}},
+    {{0b00000000, 0b00000000, 0b00011000, 0b00011000}, {0b00000000, 0b00000000, 0b00011000, 0b00011000}, {0b00000000, 0b00000000, 0b00011000, 0b00011000}, {0b00000000, 0b00000000, 0b00011000, 0b00011000}},
+    {{0b00000000, 0b00010000, 0b00011000, 0b00001000}, {0b00000000, 0b00000000, 0b00011000, 0b00110000}, {0b00000000, 0b00010000, 0b00011000, 0b00001000}, {0b00000000, 0b00000000, 0b00011000, 0b00110000}},
+    {{0b00000000, 0b00001000, 0b00011000, 0b00010000}, {0b00000000, 0b00000000, 0b00011000, 0b00001100}, {0b00000000, 0b00001000, 0b00011000, 0b00010000}, {0b00000000, 0b00000000, 0b00011000, 0b00001100}},
+    {{0b00000000, 0b00000000, 0b00001000, 0b00011100}, {0b00000000, 0b00001000, 0b00001100, 0b00001000}, {0b00000000, 0b00000000, 0b00011100, 0b00001000}, {0b00000000, 0b00001000, 0b00011000, 0b00001000}}
+    
+    
+    
+    
+    
+    
+    
+    /*
     {0b00000000, 0b00001000, 0b00001000, 0b00011000}, //J
     {0b00000000, 0b00010000, 0b00010000, 0b00011000}, //L
     {0b00010000, 0b00010000, 0b00010000, 0b00010000}, //I
@@ -12,11 +27,14 @@ const char figures[7][4] =
     {0b00000000, 0b00010000, 0b00011000, 0b00001000}, //4
     {0b00000000, 0b00001000, 0b00011000, 0b00010000}, //z
     {0b00000000, 0b00000000, 0b00010000, 0b00111000}, //inverted T
+     */
 };
  
-char a = 4;
+char a = 3;
 char temp[4] = {0,0,0,0 }, pow;
-
+char fig = 0;
+char rotation = 0;
+char position = 0;
 char random_number(char max_number){
         srand((a)); 
         a+=5;
@@ -26,10 +44,14 @@ char random_number(char max_number){
 
 
 void choose_new_figure(char* fig_bin_array){
-    const char *pom = figures[random_number(7)];
+    fig = random_number(7);
+    rotation = random_number(4);
+    position = 0;
+    const char *pom = figures[fig][rotation];
     for(char i=0; i<4; i++){
         fig_bin_array[i] = pom[i];
     }
+    
 }
 
 void prepare_new_figure(char* matrix, char* fig_bin_array){
@@ -53,14 +75,24 @@ __bit can_go_right(char* matrix, char* matrix_row, char *matrix_col, char* fig_b
     }
     return 1;
 }
-
+__bit can_go_right_rot(char* matrix, char* matrix_row, char* fig_bin_array){
+    for(int i = 0; i < 4; i++){
+        if(((matrix[*matrix_row - 3 + i] & (1<<7))) || ((matrix[*matrix_row - i] ^ fig_bin_array[3-i])&temp[3-i]*2)) return 0;
+    }
+    return 1;
+}
 __bit can_go_left(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
     for(int i = 0; i < 4; i++){
         if((matrix[*matrix_row - 3 + i] & (1<<0)) || ((matrix[*matrix_row - i] ^ fig_bin_array[3-i])&fig_bin_array[3-i]/2)) return 0;
     }
     return 1;
 }
-
+__bit can_go_left_rot(char* matrix, char* matrix_row, char* fig_bin_array){
+    for(int i = 0; i < 4; i++){
+        if((matrix[*matrix_row - 3 + i] & (1<<0)) || ((matrix[*matrix_row - i] ^ fig_bin_array[3-i])&temp[3-i]/2)) return 0;
+    }
+    return 1;
+}
 void go_down_1place(char* matrix, char* matrix_row, char* fig_bin_array){
     for(char i=0; i<4; i++){
         matrix[(*matrix_row)+1-i] = matrix[(*matrix_row)+1-i] | (fig_bin_array[3-i]);
@@ -76,10 +108,18 @@ void go_right(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_ar
             fig_bin_array[3-i] = fig_bin_array[3-i]*2;
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (fig_bin_array[3-i]);
         }
-        *matrix_col--;
+        position++;
     }
 }
-
+void go_right_rot(char* matrix, char* matrix_row, char* fig_bin_array){
+    if(can_go_right_rot(matrix, matrix_row, fig_bin_array)){
+        for(char i=0; i<4; i++){
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i]);
+            temp[3-i] = temp[3-i]*2;
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (temp[3-i]);
+        }
+    }
+}
 void go_left(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
     if(can_go_left(matrix, matrix_row, matrix_col, fig_bin_array)){
         for(char i=0; i<4; i++){
@@ -87,25 +127,37 @@ void go_left(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_arr
             fig_bin_array[3-i] = fig_bin_array[3-i]/2;
             matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (fig_bin_array[3-i]);
         }
-       *matrix_col++;
+        position--;
+    }
+}
+void go_left_rot(char* matrix, char* matrix_row, char* fig_bin_array){
+    if(can_go_left_rot(matrix, matrix_row, fig_bin_array)){
+        for(char i=0; i<4; i++){
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i]);
+            temp[3-i] = temp[3-i]/2;
+            matrix[(*matrix_row)-i] = matrix[(*matrix_row)-i] | (temp[3-i]);
+        }
     }
 }
 __bit can_rotate(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_array){
     if(!*matrix_col && fig_bin_array[1]) return 0;
+    rotation++;
     for(char i=0; i < 4; i++){
-            temp[i] = 0;
-        }
+            temp[i] = figures[fig][rotation%4][i];
+    }
+    char pos = position;
+    while(pos > 0){
+        if(pos == position) go_right_rot(matrix, matrix_row, fig_bin_array);
+        else go_right_rot(matrix, matrix_row, temp);
+        pos--;
+    }
+    while(pos < 0){
+        if(pos == position) go_left_rot(matrix, matrix_row, fig_bin_array);
+        else go_left_rot(matrix, matrix_row, temp);
+        pos++;
+    }
     for(char i = 0; i < 4; i++){
-            for(char j = 0; j < 4; j++){
-                temp[3-i] = temp[3-i] * 2;
-                temp[3-i] = temp[3-i] | (fig_bin_array[j] & (1 << ((*matrix_col) +3- i)));
-            }
-            pow = (*matrix_col) - i;
-            while(pow != 0){
-                temp[3-i] = temp[3-i] / 2;
-                pow = pow - 1;
-            }
-            if((matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i])) & temp[3-i]) return 0;
+        if((matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i])) & temp[3-i]) return 0;
     }
     return 1;
 }
@@ -113,31 +165,8 @@ void rotate(char* matrix, char* matrix_row, char *matrix_col, char* fig_bin_arra
     if(can_rotate(matrix, matrix_row, matrix_col, fig_bin_array)){
         for(char i=0; i < 4; i++){
             matrix[(*matrix_row)-i] = (matrix[(*matrix_row)-i] ^ (fig_bin_array[3-i])) | temp[3-i];
-        } 
-        pow = 0;
-        char i=0;
-        while(!temp[3-i]) {
-            i++;
+            fig_bin_array[3-i] = temp[3-i];
         }
-        while(i < 4){
-            fig_bin_array[3-pow] = temp[3-i]; 
-            pow++;
-            i++;
-        }
-        while(pow < 4){
-            fig_bin_array[3-pow] = 0;
-            pow++;
-        }
-        /*pow = 0;
-        i = 1;
-        while(i){
-            for(char j= 0; j < 4; j++){
-                if(temp[j] & (1 << 0)) {i = 0; break;}
-                temp[j] /= 2;               
-            }
-            if(i) pow++;
-        }
-        (*matrix_col) = pow;*/
     }
 }
 void remove_full_rows(char* matrix){
@@ -150,3 +179,4 @@ void remove_full_rows(char* matrix){
             }i--;
         }
     }
+}
